60cd5810b865a0678586d3839f6ada12077e2e5e
Caden <35741667+cadentj@users.noreply.github.com>
2023-10-06 01:20:59 -0400

    Color Customization

--- src/ForceGraph.js
+++ src/ForceGraph.js
@@ -5,6 +5,13 @@ import { useCallback, useMemo, useState } from "react";
 import SpriteText from "three-spritetext";
 
 
+const BACKGROUNDCOLOR = "#000000"; 
+const LINKCOLOR = "#D3D3D3";       
+const NODECOLOR = "#e6194B";      
+const HOVEREDTARGETNODECOLOR = "#4363d8";  
+const HOVEREDSOURCENODECOLOR = "#3cb44b"; 
+const MATERIALCOLOR = '#000000'
+
 function genRandomTree(N = 300, reverse = false) {
     return {
         nodes: [...Array(N).keys()].map(i => ({ id: i })),
@@ -126,9 +133,9 @@ export default function ForceGraph({ selected, setSelected, setSLink }) {
         ctx.beginPath();
         ctx.arc(node.x, node.y, NODE_R * 1.4, 0, 2 * Math.PI, false);
         if (node === hoverNode || node === clickedNode) {
-            ctx.fillStyle = 'red';
+            ctx.fillStyle = HOVEREDTARGETNODECOLOR;
         } else {
-            ctx.fillStyle = 'orange';
+            ctx.fillStyle = HOVEREDSOURCENODECOLOR;
         }
         ctx.fill();
     }, [hoverNode, clickedNode]);
@@ -155,6 +162,10 @@ export default function ForceGraph({ selected, setSelected, setSLink }) {
 
         // Graph style properties
         nodeRelSize={NODE_R}
+        backgroundColor={BACKGROUNDCOLOR}
+        nodeColor={node => 'red'}
+        linkColor={link => LINKCOLOR}
+        linkDirectionalParticleColor={MATERIALCOLOR}
         linkWidth={link => highlightLinks.has(link) ? 5 : 1}
         linkDirectionalParticles={4}
         linkDirectionalParticleWidth={link => selectedLinks.has(link) ? 8 : 0}
